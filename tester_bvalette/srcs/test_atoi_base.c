a4fb06fbc2eb93f903301591e17a1a52d5b65f8f
Brian on VM <bvalette@student.42.fr>
2020-06-21 18:09:55 +0200

    ft_atoi_base done

--- tester_bvalette/srcs/test_atoi_base.c
+++ tester_bvalette/srcs/test_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: bvalette <bvalette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 18:27:47 by bvalette          #+#    #+#             */
-/*   Updated: 2020/06/21 17:01:56 by bvalette         ###   ########.fr       */
+/*   Updated: 2020/06/21 17:49:36 by bvalette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,20 @@
 
 void test_ft_atoi_base()
 {
+	int ret = 0;
+
+
+//  [expected return], [ft_atoi_base str arg], [ft_atoi_base base arg]
 	char *test[] = 
 	{
+		"1346", "542", "0123456789ABCDEF",
 		"66", "	42", "0123456789ABCDEF",
 		"66", "42", "0123456789ABCDEF",
-		"42", "   +2A2A   e3", "0123456789ABCDEF",
+		"1346", " 542", "0123456789ABCDEF",
+		"1346", "      542 ", "0123456789ABCDEF",
+		"1346", " +542", "0123456789ABCDEF",
+		"1346", "+542", "0123456789ABCDEF",
+		"42", "   +2A   e3", "0123456789ABCDEF",
 		"1", "   1", "0123456789ABCDEF",
 		"1", "   -1", "0123456789ABCDEF",
 		"5", " xyz", "xyz",
@@ -36,28 +45,29 @@ void test_ft_atoi_base()
 		"0", "xxx", "xyz-",
 		"0", "xxx", "x+yz",
 		"0", "xxx", "x yz",
-		"0", "", "0123456789ABCDEF"
+		"0", "", "0123456789ABCDEF",
+		"0", NULL, "0123456789ABCDEF",
+		"0", "42", NULL
 	};
 	
 	int nb_test = sizeof (test) / 8 / 3;
-	int ret = 0;
 	int expected_ret = 0;
 
 	for (int i = 0; i < nb_test * 3; i += 3)
 	{
 		expected_ret =  atoi(test[i]);
 		ret =  ft_atoi_base(test[i + 1], test[i + 2]);
-		printf(DARK"Test #%2d : [%14s] BASE [%16s]"RESET, i / 3, test[i + 1], test[i + 2]);
+		printf(DARK"Test #%2d : [%-14s] BASE [%16s]"RESET, i / 3, test[i + 1], test[i + 2]);
 		if (expected_ret == ret)
 		{
 			printf(GREEN);
-			printf("expecting [%2d] return = %2d --> ", expected_ret, ret);
+			printf("expecting [%5d] return = %5d --> ", expected_ret, ret);
 			printf("[✅]\n");
 		}
 		else
 		{
 			printf(RED);
-			printf("expecting [%2d] return = %2d --> ", expected_ret, ret);
+			printf("expecting [%5d] return = %5d --> ", expected_ret, ret);
 			printf("[⛔️KO !]\n");
 		}
 		printf(RESET);
